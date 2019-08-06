04be01f3360eff25ac85a9d411073c5737ae8b02
HMBond <hubertusmariabond@gmail.com>
2019-08-06 16:32:47 +0200

    nav styling - click select / current page for navitems on mobile

--- components/styles/NavItem.js
+++ components/styles/NavItem.js
@@ -11,9 +11,9 @@ const NavItemButton = styled.div`
   width: max-content;
   align-self: center;
   cursor: pointer;
+  background-color: ${props =>
+    props.selected ? "rgba(0,0,0,0.2)" : "transparent"};
   @media only screen and (min-width: 960px) {
-    background-color: ${props =>
-      props.selected ? "rgba(0,0,0,0.2)" : "transparent"};
     :nth-child(odd) {
       align-self: flex-end;
     }
@@ -27,7 +27,11 @@ const NavItem = ({ item, selected, select }) => {
   const { name, url } = item;
   return (
     <NavItemLink href={url || "/"}>
-      <NavItemButton selected={selected} onMouseOver={() => select()}>
+      <NavItemButton
+        selected={selected}
+        onMouseOver={() => select()}
+        onClick={() => select()}
+      >
         {name}
       </NavItemButton>
     </NavItemLink>
