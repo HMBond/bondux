07158e8b497baa602f7d51760f1668972ee7033e
HMBond <hubertusmariabond@gmail.com>
2019-08-17 17:04:00 +0530

    one eye daft

--- components/styles/Logo.js
+++ components/styles/Logo.js
@@ -1,5 +1,8 @@
+import { Component } from "react";
 import styled from "styled-components";
 import LogoIcon from "../../static/bondux.svg";
+import { TimelineLite } from "gsap/umd/TweenMax";
+import debounce from "lodash/debounce";
 
 const StyledLogo = styled(({ extra, light, ...rest }) => (
   <LogoIcon {...rest} />
@@ -32,8 +35,43 @@ const StyledLogo = styled(({ extra, light, ...rest }) => (
   }
 `;
 
-const Logo = props => {
-  return <StyledLogo {...props} />;
-};
+class Logo extends Component {
+  constructor(props) {
+    super(props);
+    this.logoTween = null;
+  }
+  moveEye = debounce(
+    (className, left, top) => {
+      this.logoTween.to(className, 0.5, {
+        rotation: (Math.atan2(top, left) * 180) / Math.PI,
+        transformOrigin: "5px 50%"
+      });
+    },
+    100,
+    { leading: false, trailing: true }
+  );
+
+  componentDidMount() {
+    this.logoTween = new TimelineLite({ paused: false });
+    document.addEventListener("mousemove", event => {
+      let x = event.pageX;
+      let y = event.pageY;
+      let eye = document.getElementsByClassName("bondux_svg__left-eye")[0];
+      let offsets = eye.getBoundingClientRect();
+      let left = x - offsets.left;
+      let top = y - offsets.top;
+      console.log({ left, top });
+      this.moveEye(".bondux_svg__left-eye", left, top);
+    });
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("mousemove");
+  }
+
+  render() {
+    return <StyledLogo {...this.props} />;
+  }
+}
 
 export default Logo;
