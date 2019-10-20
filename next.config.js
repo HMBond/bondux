5d138ed98c8248ef3e9da7f1eaad247be3d51fa2
HMBond <hubertusmariabond@gmail.com>
2019-10-20 16:32:36 +0700

    Next.config.js new layout

--- next.config.js
+++ next.config.js
@@ -1,19 +1,16 @@
-const webpack = require("webpack");
-
 module.exports = {
   devIndicators: {
     autoPrerender: false
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"]
-    });
+  webpack: function(config) {
     config.module.rules.push({
       test: /\.ya?ml$/,
       use: "js-yaml-loader"
     });
-
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
     return config;
   }
 };
