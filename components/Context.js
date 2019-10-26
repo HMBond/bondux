4cd0b4d591f9cd3393aaa0d2caf100199ef63ad1
HMBond <hubertusmariabond@gmail.com>
2019-10-26 13:06:31 +0800

    context format with separate functions + navigate back/forward

--- components/Context.js
+++ components/Context.js
@@ -2,7 +2,7 @@ import { Component, createContext } from "react";
 import Router from "next/router";
 import debounce from "lodash/debounce";
 import content from "../content.yml";
-
+import constrain from "./helpers/constrain";
 const Context = createContext();
 
 export default Context;
@@ -12,65 +12,99 @@ export class ContextProvider extends Component {
     super();
     this.state = {
       devMode: false,
-      toggleDevMode: () =>
-        this.setState(prevstate => ({
-          ...prevstate,
-          devMode: !prevstate.devMode
-        })),
+      toggleDevMode: () => this.toggleDevMode(),
       selector: {
         position: 0,
-        setPosition: newPosition => {
-          if (content[newPosition]) {
-            this.setState(prevstate => ({
-              ...prevstate,
-              selector: {
-                ...prevstate.selector,
-                position: newPosition
-              }
-            }));
-          }
-        },
-        go: () => {
-          this.setState(prevstate => {
-            Router.push(content[prevstate.selector.position].url);
-            return {
-              ...prevstate,
-              nav: {
-                ...prevstate.nav,
-                open: false
-              }
-            };
-          });
-        }
+        setPosition: newPosition => this.setSelectorPos(newPosition),
+        go: () => this.visitSelectionAndCloseNav()
       },
 
       nav: {
-        back: () => Router.back(),
-        forward: () => console.log("next page"),
+        currentPath: "/",
+        currentPos: 0,
+        back: () => this.navigate(-1),
+        forward: () => this.navigate(1),
         open: false,
-        setOpen: debounce(
-          open =>
-            this.setState(prevstate => ({
-              ...prevstate,
-              nav: {
-                ...prevstate.nav,
-                open: open
-              }
-            })),
-          400,
-          { leading: true, trailing: false }
-        )
+        setOpen: bool => this.setNavOpen(bool)
       },
       content
     };
   }
 
   componentDidMount() {
-    window.context = this.state;
+    this.setNavCurrentPos();
   }
 
   render() {
     const { children } = this.props;
     return <Context.Provider value={this.state}>{children}</Context.Provider>;
   }
+
+  setNavCurrentPos() {
+    this.setState(prevState => ({
+      ...prevState,
+      nav: {
+        ...prevState.nav,
+        currentPath: window.location.pathname,
+        currentPos: content.indexOf(
+          content.find(page => page.url === window.location.pathname)
+        )
+      }
+    }));
+  }
+
+  toggleDevMode = () => {
+    this.setState(prevState => ({
+      ...prevState,
+      devMode: !prevState.devMode
+    }));
+  };
+
+  setSelectorPos = newPosition => {
+    if (content[newPosition]) {
+      this.setState(prevState => ({
+        ...prevState,
+        selector: {
+          ...prevState.selector,
+          position: newPosition
+        }
+      }));
+    }
+  };
+
+  visitSelectionAndCloseNav = () => {
+    this.setState(prevState => {
+      Router.push(content[prevState.selector.position].url);
+      return {
+        ...prevState,
+        nav: {
+          ...prevState.nav,
+          open: false
+        }
+      };
+    });
+  };
+
+  navigate = advance => {
+    this.setNavCurrentPos();
+    this.setState(prevState => {
+      Router.push(
+        content[constrain(prevState.nav.currentPos + advance, content.length)]
+          .url
+      );
+    });
+  };
+
+  setNavOpen = debounce(
+    bool =>
+      this.setState(prevState => ({
+        ...prevState,
+        nav: {
+          ...prevState.nav,
+          open: bool
+        }
+      })),
+    400,
+    { leading: true, trailing: false }
+  );
 }
