fdc7ccb61757062f5b5f644f2a215c5ab07127ab
HMBond <hubertusmariabond@gmail.com>
2019-10-28 17:49:45 +0800

    NavItem animation only for desktop

--- components/styles/NavItem.js
+++ components/styles/NavItem.js
@@ -6,13 +6,15 @@ const NavItemButton = styled.div`
   color: ${props => props.theme.colors.bg};
   font-size: 2.8rem;
   padding: 10px;
-  transform: ${props => (props.selected ? "rotateX(-190deg)" : "")};
-  transition: transform 0.2s linear;
   width: max-content;
   align-self: center;
   cursor: pointer;
   background-color: ${props =>
     props.selected ? props.theme.colors.lighten : "transparent"};
+  @media screen and (min-width: 600px) {
+    transform: ${props => (props.selected ? "rotateX(-190deg)" : "")};
+    transition: transform 0.2s linear;
+  }
 `;
 
 const NavItem = ({ item, selected, select }) => {
