5d2bd1c1c7babffbf98f63a6e76d3f8d42da7617
HMBond <hubertusmariabond@gmail.com>
2019-11-02 22:16:00 +0800

    repositioning category label

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -6,7 +6,6 @@ import uniqid from "uniqid";
 const BlogPostBase = styled.div`
   order: ${props => (props.card ? "" : "-1")};
   max-width: ${props => props.theme.maxWidth};
-  position: relative;
   display: flex;
   flex-direction: column;
   margin: 2rem 0 4rem 0;
@@ -26,6 +25,7 @@ const BlogPostBase = styled.div`
 `;
 
 const BlogImage = styled.div`
+  position: relative;
   background: url(${props => props.src});
   background-size: cover;
   background-position: center;
@@ -63,6 +63,29 @@ const BlogImage = styled.div`
     `};
 `;
 
+const Category = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  color: ${props => props.theme.colors.primary};
+  font-weight: bolder;
+  font-style: all-caps;
+  padding: 1rem;
+  height: 3rem;
+  background: ${props => props.theme.colors.lightGrey};
+  &:before {
+    content: " ";
+    width: 0;
+    height: 0;
+    position: absolute;
+    bottom: 0;
+    right: 99.98%;
+    border-top: 3rem solid transparent;
+    border-left: 3rem solid transparent;
+    border-bottom: 3rem solid ${props => props.theme.colors.lightGrey};
+  }
+`;
+
 const BlogTitle = styled(Title)`
   margin-top: 2rem;
 `;
@@ -85,6 +108,11 @@ const Paragraph = styled.div`
     font-family: "DejaVu Serif";
     color: ${props => props.theme.colors.primary + "80"};
   }
+  & *:not(p):first-letter {
+    font-size: inherit;
+    font-family: inherit;
+    color: inherit;
+  }
 `;
 
 const BlogText = styled.p`
@@ -95,36 +123,14 @@ const BlogText = styled.p`
   }
 `;
 
-const Category = styled.div`
-  position: absolute;
-  right: 0;
-  top: 0;
-  color: ${props => props.theme.colors.bg};
-  font-weight: bolder;
-  padding: 1rem;
-  height: 3rem;
-  background: ${props => props.theme.colors.grey};
-  &:before {
-    content: " ";
-    width: 0;
-    height: 0;
-    position: absolute;
-    top: 0;
-    right: 99.98%;
-    border-left: 3rem solid transparent;
-    border-bottom: 3rem solid transparent;
-    border-top: 3rem solid ${props => props.theme.colors.grey};
-  }
-`;
-
 const BlogPost = ({ card, entry, ...props }) => {
   if (card) {
     return (
       <BlogPostBase card {...props}>
         {entry.topImgUrl && (
           <BlogImage src={entry.topImgUrl} card {...props}>
-            {entry.category && <Category>{entry.category}</Category>}
             {entry.title && <BlogTitle>{entry.title}</BlogTitle>}
+            {entry.category && <Category>{entry.category}</Category>}
             {entry.summary && (
               <BlogSummary>
                 <span>{entry.summary}</span>
