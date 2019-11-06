c246a56977cc07b2c2a294ab123feb70cc86cb95
HMBond <hubertusmariabond@gmail.com>
2019-11-06 17:50:21 +0800

    navitem thin without lightbox

--- components/styles/NavItem.js
+++ components/styles/NavItem.js
@@ -2,15 +2,13 @@ import styled from "styled-components";
 import NavItemLink from "next/link";
 
 const NavItemButton = styled.div`
-  font-family: "DejaVu Condensed Bold";
   color: ${props => props.theme.colors.bg};
   font-size: 2.8rem;
   padding: 10px;
   width: max-content;
   align-self: center;
   cursor: pointer;
-  background-color: ${props =>
-    props.selected ? props.theme.colors.lighten : "transparent"};
+  font-family: ${props => (props.selected ? "DejaVu Condensed Bold" : "unset")};
   @media screen and (min-width: 600px) {
     transform: ${props => (props.selected ? "rotateX(-190deg)" : "")};
     transition: transform 0.2s linear;
